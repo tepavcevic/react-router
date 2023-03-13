185b10dc07bc4e7aa0204a0823c4f4e2e8f0c125
tepavcevic <djordje.tepa@gmail.com>
2023-03-13 15:13:16 +0100

    Fixed indentation

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout() {
                 <ul>
                     <li>
                         <Link to="/">Home</Link>
-                        </li>
+                    </li>
                     <li>
                         <Link to="/about">About</Link>
                     </li>
